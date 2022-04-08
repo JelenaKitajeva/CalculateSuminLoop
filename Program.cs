d4677aa9a02ac9c3662c79b889d0e75320a78774
Jelena Kitajeva <lena_terehhova@mail.ru>
2022-04-08 10:26:31 +0300

    calculate

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CalculateLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int myRandom = 0;
+            int Sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                myRandom = rnd.Next(1, 11);
+                Sum = Sum + myRandom;
+                Console.WriteLine($"My random number:{ myRandom}");
+
+            }
+
+            Console.WriteLine($"Sum={Sum}", Sum);
+
+        }
+    }
+}
